12355317a5b1762bcbde260d0d02a6e559c01171
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2026-03-08 03:49:37 -0400

    change profile version

--- _validate_workflow_yaml.py
+++ _validate_workflow_yaml.py
@@ -3,4 +3,5 @@ import yaml
 path = Path('/mnt/c/Users/Makin/Desktop/Proton build/proton-arm64-nightlies/.github/workflows/proton-nightly-build.yml')
 obj = yaml.safe_load(path.read_text(encoding='utf-8'))
 print('loaded:', type(obj).__name__)
-print('force line ok')
+for item in obj['jobs']['build']['strategy']['matrix']['include']:
+    print(item['arch_id'], item['profile_version'])
